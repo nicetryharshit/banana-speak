584e73c48c01892100695965053d55ffe00ec451
nicetryharshit <harshitdubeyhd@gmail.com>
2022-08-15 22:31:00 +0530

    functioning app no styling

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
+var inputTextArea = document.querySelector("#txt-area-input");
+var outputTextArea = document.querySelector("#txt-area-output");
 var btnTranslate = document.querySelector("#btn-translate");
-btnTranslate.addEventListener("click", buttonClicked());
 
-function buttonClicked() {
-    console.log("CLICKED");
+var baseURL = "https://api.funtranslations.com/translate/minion.json";
+
+function generateURL(inputText) {
+    return baseURL + "?" + "text=" + inputText;
+}
+
+function displayOutput(json) {
+    var translation = json.contents.translated;
+    outputTextArea.innerText = translation;
+    btnTranslate.disabled = false;
+    btnTranslate.innerText = "Translate";
+}
+
+function handleError(error) {
+    alert(error.value);
+    btnTranslate.disabled = false;
+    btnTranslate.innerText = "Translate";
+
+}
+btnTranslate.addEventListener("click", () => onTranslate());
+
+function onTranslate() {
+    btnTranslate.disabled = true;
+    btnTranslate.innerText = "Translating...";
+    var inputText = inputTextArea.value;
+    fetch(generateURL(inputText))
+        .then(response => response.json())
+        .then(json => displayOutput(json))
+        .catch(handleError)
 }
